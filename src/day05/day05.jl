52cd52e15f730756186a7646f2d1eaf9611b2e77
Diogo Nunes Carvalho <74015980+diogonunescarv@users.noreply.github.com>
2023-12-25 18:35:53 -0300

    starting day05

--- src/day05/day05.jl
+++ src/day05/day05.jl
@@ -8,20 +8,66 @@ function readInput(path::String)
 end
 
 findNumber(s::AbstractString) = parse(Int, String([x for x in identity(s) if isdigit(x)]))
+transformArray(s::AbstractString) = (split(s," "))
 
-transformArray(s::AbstractString) = split(s," ")
+# function findSeeds(s::AbstractString)
+#     st, nb = split(s,"seeds:")
+#     seed = parse.(Int,filter(x -> strip(x) != "",transformArray(nb)))
+#     return seed
+# end 
+
+function numericLine(line::AbstractString)
+    parts = split(line, " ")
+    for part in parts
+        number = parse.(Int, part, nothing)
+        if isnothing(number)
+            return false
+        end
+    end
+    return true
+end
+
+function modSeeds(seeds::Vector{Int}, strt::Int, fnl::Int, pace::Int)
+    println(strt," " , fnl, " ",pace)
+    global i = 1
+    for s in seeds 
+        if s in fnl:(fnl+pace-1)
+            seeds[i] = s - fnl + strt
+        end
+        global i += 1
+    end
+    #println(seeds)
+    return seeds
+end 
 
 # ============================================================================================
 # Main
 
-input::String = readInput("c:\\Users\\User\\Desktop\\Codes\\aoc-2023\\input\\tst-day05.txt")
+input::String = readInput("c:\\Users\\User\\Desktop\\Codes\\aoc-2023\\input\\day05.txt")
 
 lines = split(rstrip(input), "\n")
 
+global seeds = parse.(Int, split(strip(split(lines[1], ":")[2]), " "))
+
+data = Dict{Tuple{String,String},Vector{Int}}()
+
+for line in lines[2:end]
+    if (line != "\n" && strip(line) != "" && occursin("map", line) === false)
+        transf = parse.(Int,filter(x -> strip(x) != "",transformArray(line)))
+        #println(transf)
+        global seeds = modSeeds(seeds, transf[1], transf[2], transf[3])
+        println(seeds) 
+    end 
+end
+
 # Part 1 
 
-getSeeds()
+# global seeds 
+# global first = true 
 
-for line in lines 
-    println(line)
-end 
+# global i = 1
+# for line in lines  
+
+# end 
+
+println("Part 1: ", minimum(seeds))
