18f6770be879074284ce30fd7d23818fcd469d0b
Diogo Nunes Carvalho <74015980+diogonunescarv@users.noreply.github.com>
2024-01-11 17:33:27 -0300

    day 05 - part 2 incompleted

--- src/day05/day05.jl
+++ src/day05/day05.jl
@@ -9,13 +9,7 @@ end
 
 findNumber(s::AbstractString) = parse(Int, String([x for x in identity(s) if isdigit(x)]))
 transformArray(s::AbstractString) = (split(s," "))
-
-# function findSeeds(s::AbstractString)
-#     st, nb = split(s,"seeds:")
-#     seed = parse.(Int,filter(x -> strip(x) != "",transformArray(nb)))
-#     return seed
-# end 
-
+ 
 function numericLine(line::AbstractString)
     parts = split(line, " ")
     for part in parts
@@ -28,18 +22,29 @@ function numericLine(line::AbstractString)
 end
 
 function modSeeds(seeds::Vector{Int}, strt::Int, fnl::Int, pace::Int)
-    #println(strt," " , fnl, " ",pace)
     global i = 1
     for s in seeds 
         if s in fnl:(fnl+pace-1) && !(i in positionsChanged) 
             seeds[i] = s - fnl + strt
-            push!(positionsChanged, i)
+            append!(positionsChanged, i)
         end
         global i += 1
     end
-    #println(seeds)
     return seeds
 end 
+
+function generateSeeds(seeds::Vector{Int})
+    newSeeds = Int[]
+
+    for i in 1:2:length(seeds)-1
+        start = seeds[i]
+        count = seeds[i+1]
+        ns = start:start + count - 1
+        append!(newSeeds, ns)
+    end
+    println("New Arraw: ", newSeeds)
+    return newSeeds
+end
 
 # ============================================================================================
 # Main
@@ -50,6 +55,8 @@ lines = split(rstrip(input), "\n")
 
 global seeds = parse.(Int, split(strip(split(lines[1], ":")[2]), " "))
 
+global seeds2 = generateSeeds(seeds)
+
 data = Dict{Tuple{String,String},Vector{Int}}()
 
 positionsChanged = []
@@ -57,9 +64,8 @@ positionsChanged = []
 for line in lines[2:end]
     if (line != "\n" && strip(line) != "" && occursin("map", line) === false)
         transf = parse.(Int,filter(x -> strip(x) != "",transformArray(line)))
-        #println(transf)
         global seeds = modSeeds(seeds, transf[1], transf[2], transf[3])
-        #println(seeds)
+        #global seeds2 = modSeeds(seeds2, transf[1], transf[2], transf[3])
     else  
         deleteat!(positionsChanged, 1:length(positionsChanged)) 
     end 
@@ -68,3 +74,4 @@ end
 # Part 1 
 
 println("Part 1: ", minimum(seeds))
+println("Part 2: ", minimum(seeds2)) #not working yet
